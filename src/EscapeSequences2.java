2494c28c35ea25c78b5910732e5ea36581b7d183
npanch6468 <nagapanchumarty@gmail.com>
2019-06-12 11:39:37 -0400

    first commit

---
+++ src/EscapeSequences2.java
@@ -0,0 +1,9 @@
+public class EscapeSequences2 {
+    public static void main(String [] args){
+        System.out.println("Cost\t Quantity\tTotal");
+        System.out.println("$1,000.00\t4\t  $4,000.00");
+        System.out.println("$   50.00\t8\t    $400.00");
+        System.out.println("_________________________");
+        System.out.println("Total:\t   12\t  $4,400.00");
+    }
+}
